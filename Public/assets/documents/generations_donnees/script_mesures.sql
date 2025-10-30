2aa49de7693bae1fd1d4bd772b2530710c199d76
GHEUX Théo <theoxghx@gmail.com>
2025-10-30 12:37:36 +0100

    Ajout des mesures des 50 patients + 1 exemple de valeurs pour les mesures du patient 1

---
+++ Public/assets/documents/generations_donnees/script_mesures.sql
@@ -0,0 +1,190 @@
+DELETE FROM MESURES;
+
+INSERT INTO MESURES (id_mesure, pt_id, type_mesure, unite)
+VALUES (1,1,'Température corporelle','°C'),
+       (2,1,'Tension arterielle','mmHg'),
+       (3,1,'Fréquence cardiaque','bpm'),
+
+       (4, 2, 'Température corporelle', '°C'),
+       (5, 2, 'Tension artérielle', 'mmHg'),
+       (6, 2, 'Fréquence cardiaque', 'bpm'),
+
+       (7, 3, 'Température corporelle', '°C'),
+       (8, 3, 'Poids', 'kg'),
+       (9, 3, 'Fréquence cardiaque', 'bpm'),
+
+       (10, 4, 'Tension artérielle', 'mmHg'),
+       (11, 4, 'Fréquence cardiaque', 'bpm'),
+       (12, 4, 'Glycémie', 'mmol/L'),
+
+       (13, 5, 'Température corporelle', '°C'),
+       (14, 5, 'Tension artérielle', 'mmHg'),
+       (15, 5, 'Poids', 'kg'),
+       (16, 5, 'Glycémie', 'mmol/L'),
+
+       (17, 6, 'Température corporelle', '°C'),
+       (18, 6, 'Fréquence cardiaque', 'bpm'),
+       (19, 6, 'Poids', 'kg'),
+
+       (20, 7, 'Température corporelle', '°C'),
+       (21, 7, 'Tension artérielle', 'mmHg'),
+       (22, 7, 'Fréquence cardiaque', 'bpm'),
+
+       (23, 8, 'Poids', 'kg'),
+       (24, 8, 'Glycémie', 'mmol/L'),
+       (25, 8, 'Fréquence cardiaque', 'bpm'),
+
+       (26, 9, 'Température corporelle', '°C'),
+       (27, 9, 'Tension artérielle', 'mmHg'),
+
+       (28, 10, 'Tension artérielle', 'mmHg'),
+       (29, 10, 'Poids', 'kg'),
+       (30, 10, 'Glycémie', 'mmol/L'),
+
+       (31, 11, 'Température corporelle', '°C'),
+       (32, 11, 'Tension artérielle', 'mmHg'),
+       (33, 11, 'Fréquence cardiaque', 'bpm'),
+
+       (34, 12, 'Poids', 'kg'),
+       (35, 12, 'Glycémie', 'mmol/L'),
+
+       (36, 13, 'Température corporelle', '°C'),
+       (37, 13, 'Fréquence cardiaque', 'bpm'),
+
+       (38, 14, 'Tension artérielle', 'mmHg'),
+       (39, 14, 'Glycémie', 'mmol/L'),
+       (40, 14, 'Poids', 'kg'),
+
+       (41, 15, 'Température corporelle', '°C'),
+       (42, 15, 'Poids', 'kg'),
+       (43, 15, 'Glycémie', 'mmol/L'),
+
+       (44, 16, 'Tension artérielle', 'mmHg'),
+       (45, 16, 'Fréquence cardiaque', 'bpm'),
+
+       (46, 17, 'Température corporelle', '°C'),
+       (47, 17, 'Poids', 'kg'),
+       (48, 17, 'Fréquence cardiaque', 'bpm'),
+
+       (49, 18, 'Température corporelle', '°C'),
+       (50, 18, 'Tension artérielle', 'mmHg'),
+
+       (51, 19, 'Poids', 'kg'),
+       (52, 19, 'Glycémie', 'mmol/L'),
+       (53, 19, 'Fréquence cardiaque', 'bpm'),
+
+       (54, 20, 'Température corporelle', '°C'),
+       (55, 20, 'Tension artérielle', 'mmHg'),
+       (56, 20, 'Poids', 'kg'),
+
+       (57, 21, 'Fréquence cardiaque', 'bpm'),
+       (58, 21, 'Poids', 'kg'),
+       (59, 21, 'Glycémie', 'mmol/L'),
+
+       (60, 22, 'Température corporelle', '°C'),
+       (61, 22, 'Tension artérielle', 'mmHg'),
+
+       (62, 23, 'Température corporelle', '°C'),
+       (63, 23, 'Poids', 'kg'),
+       (64, 23, 'Glycémie', 'mmol/L'),
+
+       (65, 24, 'Température corporelle', '°C'),
+       (66, 24, 'Tension artérielle', 'mmHg'),
+       (67, 24, 'Fréquence cardiaque', 'bpm'),
+
+       (68, 25, 'Poids', 'kg'),
+       (69, 25, 'Glycémie', 'mmol/L'),
+
+       (70, 26, 'Température corporelle', '°C'),
+       (71, 26, 'Tension artérielle', 'mmHg'),
+       (72, 26, 'Fréquence cardiaque', 'bpm'),
+
+       (73, 27, 'Température corporelle', '°C'),
+       (74, 27, 'Poids', 'kg'),
+       (75, 27, 'Fréquence cardiaque', 'bpm'),
+
+       (76, 28, 'Température corporelle', '°C'),
+       (77, 28, 'Tension artérielle', 'mmHg'),
+       (78, 28, 'Glycémie', 'mmol/L'),
+
+       (79, 29, 'Poids', 'kg'),
+       (80, 29, 'Fréquence cardiaque', 'bpm'),
+
+       (81, 30, 'Température corporelle', '°C'),
+       (82, 30, 'Tension artérielle', 'mmHg'),
+       (83, 30, 'Poids', 'kg'),
+
+       (84, 31, 'Température corporelle', '°C'),
+       (85, 31, 'Fréquence cardiaque', 'bpm'),
+
+       (86, 32, 'Tension artérielle', 'mmHg'),
+       (87, 32, 'Poids', 'kg'),
+       (88, 32, 'Glycémie', 'mmol/L'),
+
+       (89, 33, 'Température corporelle', '°C'),
+       (90, 33, 'Tension artérielle', 'mmHg'),
+       (91, 33, 'Fréquence cardiaque', 'bpm'),
+
+       (92, 34, 'Poids', 'kg'),
+       (93, 34, 'Glycémie', 'mmol/L'),
+
+       (94, 35, 'Température corporelle', '°C'),
+       (95, 35, 'Tension artérielle', 'mmHg'),
+       (96, 35, 'Poids', 'kg'),
+
+       (97, 36, 'Température corporelle', '°C'),
+       (98, 36, 'Glycémie', 'mmol/L'),
+       (99, 36, 'Fréquence cardiaque', 'bpm'),
+
+       (100, 37, 'Température corporelle', '°C'),
+       (101, 37, 'Tension artérielle', 'mmHg'),
+
+       (102, 38, 'Température corporelle', '°C'),
+       (103, 38, 'Poids', 'kg'),
+       (104, 38, 'Glycémie', 'mmol/L'),
+
+       (105, 39, 'Température corporelle', '°C'),
+       (106, 39, 'Tension artérielle', 'mmHg'),
+       (107, 39, 'Fréquence cardiaque', 'bpm'),
+
+       (108, 40, 'Poids', 'kg'),
+       (109, 40, 'Glycémie', 'mmol/L'),
+
+       (110, 41, 'Température corporelle', '°C'),
+       (111, 41, 'Tension artérielle', 'mmHg'),
+       (112, 41, 'Fréquence cardiaque', 'bpm'),
+
+       (113, 42, 'Température corporelle', '°C'),
+       (114, 42, 'Poids', 'kg'),
+
+       (115, 43, 'Température corporelle', '°C'),
+       (116, 43, 'Tension artérielle', 'mmHg'),
+       (117, 43, 'Glycémie', 'mmol/L'),
+
+       (118, 44, 'Température corporelle', '°C'),
+       (119, 44, 'Tension artérielle', 'mmHg'),
+       (120, 44, 'Fréquence cardiaque', 'bpm'),
+
+       (121, 45, 'Poids', 'kg'),
+       (122, 45, 'Fréquence cardiaque', 'bpm'),
+
+       (123, 46, 'Température corporelle', '°C'),
+       (124, 46, 'Tension artérielle', 'mmHg'),
+       (125, 46, 'Glycémie', 'mmol/L'),
+
+       (126, 47, 'Température corporelle', '°C'),
+       (127, 47, 'Fréquence cardiaque', 'bpm'),
+       (128, 47, 'Poids', 'kg'),
+
+       (129, 48, 'Tension artérielle', 'mmHg'),
+       (130, 48, 'Fréquence cardiaque', 'bpm'),
+       (131, 48, 'Glycémie', 'mmol/L'),
+
+       (132, 49, 'Température corporelle', '°C'),
+       (133, 49, 'Tension artérielle', 'mmHg'),
+       (134, 49, 'Poids', 'kg'),
+
+       (135, 50, 'Température corporelle', '°C'),
+       (136, 50, 'Tension artérielle', 'mmHg'),
+       (137, 50, 'Fréquence cardiaque', 'bpm'),
+       (138, 50, 'Glycémie', 'mmol/L');
